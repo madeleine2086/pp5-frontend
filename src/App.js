cc9287298690737655199e0afc6214b5d3296cb7
Magdalena Dmitruk <dmitrukm@gmail.com>
2024-06-29 18:18:44 +0000

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import styles from '../src/App.module.css';
 import React from 'react';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
